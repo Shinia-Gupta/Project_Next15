37ab24fa73aa5138ed83ff18f033bc020bea837f
Shinia-Gupta <shiniavit@gmail.com>
2025-10-15 11:56:35 +0530

    mandatory catch all segments with [...slug]

---
+++ src/app/docs/[...slug]/page.tsx
@@ -0,0 +1,10 @@
+export default function Docs({params}: {params: {slug: string[]}}) {
+    const {slug = []} = params
+    console.log('slug:', slug)
+    if(slug?.length===2){
+        return <div>Docs Page for feature - {slug[0]} and concept - {slug[1]}</div>
+    }else if(slug?.length===1){
+        return <div>Docs Page for feature - {slug[0]}</div>
+    }
+    return <div>Docs Home Page</div>
+}
