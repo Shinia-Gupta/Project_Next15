3d7e85f1b0d12e264daa10d511422487c17fe68b
Shinia-Gupta <shiniavit@gmail.com>
2025-10-15 12:55:21 +0530

    Route Groups in nextjs

---
+++ src/app/(auth)/forgot-password/page.tsx
@@ -0,0 +1,7 @@
+export default function ForgotPassword() {
+  return (
+    <main>
+      <h1>Welcome to Forgot password!</h1>
+    </main>
+  );
+}
---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,7 @@
+export default function Register() {
+  return (
+    <main>
+      <h1>Welcome to Register!</h1>
+    </main>
+  );
+}
