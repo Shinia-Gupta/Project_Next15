531e9882d34032857199f4e4f5e64a574e3f01e8
Shinia-Gupta <shiniavit@gmail.com>
2025-10-15 18:28:22 +0530

    Link Component Navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Welcome to Home!</h1>
+      <Link href="/products">Go to Products</Link><br/>
+      <Link href="/blog">Go to Blog</Link>
+
     </main>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+
 export default function Products() {
+  let prodId="100";
   return (
     <main>
       <h1>Welcome to Products!</h1>
+      <Link href={"/"}>Home</Link><br/>
+      <Link href={"/products/1"}>Product 1</Link><br/>
+      <Link href={"/products/2"}>Product 2</Link><br/>
+      <Link href={"/products/3"} replace>Product 3</Link><br/>  {/* Navigates back to the home page instead of this product list page */}
+      <Link href={`/products/${prodId}`}>Product {prodId}</Link>
     </main>
   );
 }
