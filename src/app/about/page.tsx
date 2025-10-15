b45cd263f137c88dd4ce8760c9773bea6c8db6b5
Shinia-Gupta <shiniavit@gmail.com>
2025-10-15 16:33:32 +0530

    static metadata continued

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,6 +14,17 @@
 //(in that case metadata can use anything which is available at request time)
 //(in that case metadata can be different for different requests)
 
+
+// If we look in the elements tab of dev tools
+// we can see the title of this page is "About Page Metadata title"
+// and not "Next.js App Router" which is the title in root layout metadata
+// because child metadata overrides parent metadata if same key is present in both
+//(in this case title key is present in both root layout metadata and about page metadata)
+
+//if we remove this metadata object from this file
+//then the title of this page will be "Next.js App Router"
+//because now only parent metadata is present
+//and child metadata is not present
 export const metadata={
   title:"About Page Metadata title"
 }
