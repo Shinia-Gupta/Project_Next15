853f196df312f30ee29e823b7dc5bea48c08e937
Shinia-Gupta <shiniavit@gmail.com>
2025-10-15 12:06:09 +0530

    optional catch all route

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -1,5 +1,5 @@
-export default function Docs({params}: {params: {slug: string[]}}) {
-    const {slug = []} = params
+export default async function Docs({params}: {params: Promise<{slug: string[]}>}) {
+    const {slug} = await params
     console.log('slug:', slug)
     if(slug?.length===2){
         return <div>Docs Page for feature - {slug[0]} and concept - {slug[1]}</div>
