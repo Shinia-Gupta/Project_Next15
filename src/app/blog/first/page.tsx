f1ff808531246a51e5692d6bbeb300cf44b397c3
Shinia-Gupta <shiniavit@gmail.com>
2025-10-15 11:01:28 +0530

    nested routes

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,7 @@
+export default function FirstBlog() {
+  return (
+    <main>
+      <h1>Welcome to First Blog!</h1>
+    </main>
+  );
+}
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,7 @@
+export default function SecondBlog() {
+  return (
+    <main>
+      <h1>Welcome to Second Blog!</h1>
+    </main>
+  );
+}
