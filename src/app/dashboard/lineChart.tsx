e3e91fb49ad6ac0b94d16911526b1daeb1f10230
Shinia-Gupta <shiniavit@gmail.com>
2025-10-15 12:32:59 +0530

    File colocation example- no other file other than the page.tsx got rendered by the browser accidently

---
+++ src/app/dashboard/lineChart.tsx
@@ -0,0 +1,7 @@
+export default function LineChart() {
+  return (
+    <main>
+      <h1>Line Chart page</h1>
+    </main>
+  );
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <main>
+      <h1>Dashboard page - File Colocation example. No other files in this route got rendered except the page.tsx file</h1>
+    </main>
+  );
+}
