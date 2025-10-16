3084f077d927f1a59f13d7563ed44fd7412755fb
Shinia-Gupta <shiniavit@gmail.com>
2025-10-16 15:13:38 +0530

    simple error boundary

--- src/app/products/[id]/reviews/[reviewId]/page.tsx
+++ src/app/products/[id]/reviews/[reviewId]/page.tsx
@@ -1,11 +1,18 @@
 import { notFound, redirect } from "next/navigation";
 
+function getRandomInt(count:number) {
+  return Math.floor(Math.random() * count);
+}
 //Server Component
 export default async function ProductsById({
     params
 }:{
   params:Promise<{id:string,reviewId:string}>
 }) {
+  const random=getRandomInt(2)
+  if(random===1){
+    throw new Error("Error loading review data");
+  }
   const {id,reviewId}=await params;
   if(parseInt(id)>1000){
     // notFound();
